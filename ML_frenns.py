f788e8df5d976a6977697944263e1b15d6d0ba58
ysf12374 <47970186+ysf12374@users.noreply.github.com>
2019-06-01 12:04:33 +0200

    Add files via upload

---
+++ ML_frenns.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 22:24:45 2019
+
+@author: Yousuf
+"""
+frenns='FRN100000570'
+reg='04378201'
+namez='BP P.L.C'
+import numpy as np
+import pandas as pd
+import mysql.connector
+import pandas as pd
+from sklearn import model_selection
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+mysql = mysql.connector.connect(
+  host="144.76.137.232",
+  user="vkingsolextadmin",
+  passwd="~O3o3y4h",
+  database = "vkingsol_frennsdevelopment"
+  )
+df=pd.read_sql('select syncinvoice_id,amount, vat_amount, outstanding_amount,paid from syncinvoice', con=mysql)
+x=pd.read_sql('select amount, vat_amount, outstanding_amount from syncinvoice', con=mysql)
+df_out=df['paid']
+#df=df['syncinvoice_id']
+df1=df['amount'].values.astype('float32')
+df2=df['vat_amount'].values.astype('float32')    
+df3=df['outstanding_amount'].values.astype('float32')    
+df_no_plot=df['syncinvoice_id'].values.astype(int)
+#df_out=[1 for i in df_out if i =='true']     +
+X=x.values.astype('float32')
+
+for i in range(0,len(df_out)):
+    if df_out[i]=='true':
+        df_out.at[i]=1      
+    elif df_out[i]=='false':
+        df_out.at[i]=0
+    else:
+        df_out.at[i]=2
+Y=df_out.values.astype(int)
+## import the necessary packages
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import SGD
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+
+validation_size = 0.11
+seed=7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+scoring = 'accuracy'
+models = []
+models.append(('LR', LogisticRegression()))
+lb = LabelBinarizer()
+Y_train = lb.fit_transform(Y_train)
+Y_validation = lb.transform(Y_validation)
+
+
+# define the 3072-1024-512-3 architecture using Keras
+model = Sequential()
+model.add(Dense(256, input_dim=3, init='uniform', activation='sigmoid'))
+#model.add(Dense(512, input_dim=3, init='normal', activation="sigmoid"))
+#model.add(Dense(10, activation="sigmoid"))
+model.add(Dense(64, activation="sigmoid"))
+#model.add(Dense(32, activation="relu"))
+model.add(Dense(3, activation="softmax"))
+
+# initialize our initial learning rate and # of epochs to train for
+INIT_LR = 0.01
+EPOCHS = 10
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+H = model.fit(X_train, Y_train, validation_data=(X_validation, Y_validation),epochs=EPOCHS, batch_size=32)
+#H=model.fit(X_train, Y_train, epochs=15, batch_size=10,  verbose=3)
+#predictions = model.predict(X_validation[0])
+scores = model.evaluate(X_validation, Y_validation)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+# calculate predictions
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(X_validation, batch_size=32)
+#print(classification_report(Y_validation.argmax(axis=1),
+#	predictions.argmax(axis=1), target_names=lb.classes_))
+
+# plot the training loss and accuracy
+N = np.arange(0, EPOCHS)
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, H.history["loss"], label="train_loss")
+plt.plot(N, H.history["val_loss"], label="val_loss")
+plt.plot(N, H.history["acc"], label="train_acc")
+plt.plot(N, H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy (Simple NN)")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.savefig("plot.png")
+# compile the model using SGD as our optimizer and categorical
+# cross-entropy loss (you'll want to use binary_crossentropy
+# for 2-class classification)
+
+#models.append(('KNN', KNeighborsClassifier(n_neighbors=3)))
+#models.append(('SVM', SVC(kernel='linear', C=0.01)))
+#results = []
+#names = []
+#for name, model in models:
+#	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+#	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+#	results.append(cv_results)
+#	names.append(name)
+#	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+#	print(msg)
+#lr = LogisticRegression()
+#lr.fit(X_train, Y_train)
+#predictions = lr.predict(X_validation)
+#print('LogisticRegression')
+#print(accuracy_score(Y_validation, predictions))
+#print(confusion_matrix(Y_validation, predictions))
+
+
+#from sklearn.cluster import KMeans
+#from matplotlib import style
+#import matplotlib.pyplot as plt
+#X=np.array([df1,df2,df3])
+#
+#X=X.transpose()
+#kmeans = KMeans(n_clusters=8)
+#kmeans.fit(X)
+#
+#centroids = kmeans.cluster_centers_
+#labels = kmeans.labels_
+#
+#print(centroids)
+#print(labels)       
+#colors = ["g.","r.","c.","y.",".b",".m",".k",".w"]
+#
+#labels_plot=labels.astype(int)
+##for i in range(0,len(X)):
+##    print("coordinate:",X[i], "label:", labels[i])
+##    plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize = 10)
+##
+##plt.scatter(centroids[:, 0],centroids[:, 1], marker = "x", s=150, linewidths = 5, zorder = 10)
+##
+##
+##pdd=df[[0]]
+#
+#
+#labels=pd.DataFrame(labels)
+#report=pd.concat([df_no,labels],axis=1)
+#for i in range(0,len(X)):
+#    plt.plot(df_no_plot[i], labels_plot[i], colors[labels_plot[i]], markersize = 10)
+#plt.show()
+#ff=pd.read_sql('select company_name from syncfinancialanalysis where company_name = "' + namez + '" and frenns_id = "' + frenns + '"', con=mysql)# 
+#updatesql=ff['company_name']
+#df=df.groupby('frenns_id').mean()
+#updsql=updatesql.as_matrix()
+#df=df.values.flatten()
+## set the matplotlib backend so figures can be saved in the background
+#import matplotlib
+#matplotlib.use("Agg")
+#
+
+#import pickle
+#import cv2
+#import os
+#
+#(trainX, testX, trainY, testY) = train_test_split(data,
+#	labels, test_size=0.25, random_state=42)
+
+###########################################################################################################
+####### These are mysql queries to select the columns from clearsight_development table  ##################
+###########################################################################################################
+
+#df = pd.read_sql('select * from company_ann_reports where CompanyNumber = "' + reg + '"', con=mysql)
+#llf=pd.read_sql('select Result from fraud_analysis where CompanyNumber="' + reg + '"', con=mysql)
+#
+#pp=pd.read_sql('select CompanyNumber from ML where CompanyNumber ="' + reg + '"', con=mysql)
+#try:
+#    updatesql=pp['CompanyNumber']
+#    updsql=len(updatesql)
+#except:
+#    updsql=0;
+#
+#df=df.drop(['URL','NAME','CompanyNumber','YEAR'],axis=1)
+#df1=df.mean().transpose()#.reset_index().drop(['index'],axis=1)
+#
+##mycursor=mysql.cursor()
+#records=[str(df1['work_cap']),str(df1['COS']),str(df1['curr_ass']),str(df1['fix_ass']),str(df1['tot_ass']),str(df1['depr_amort']),str(df1['PPE']),str(df1['SGA']),str(df1['Long_term_d']),str(df1['curr_lia']),str(df1['inc_tax']),str(df1['cash']),str(df1['sales']),str(df1['pnl_before_tax']),str(df1['pnl_after_tax']),str(df1['grossprofit']),str(llf['Result'].iloc[0])]
+#if updsql==0:
+#    sql = "INSERT INTO ML(CompanyNumber, NAME,creditrating, creditscore,creditlimit,revenue) VALUES(%s,%s,%s,%s,%s,%s)"#,combinedcreditscore,combinedpurchasescore)
+#    mycursor.execute(sql,records)
+#    mysql.commit()
+#    mycursor.close()
+#    print(records)
+#    #mysql.close()
+#else:
+#    sql="UPDATE ML SET creditrating=%s,creditscore=%s,creditlimit=%s,revenue=%s  WHERE CompanyNumber='" + reg + "'" #,combinedcreditscore=%s,combinedpurchasescore=%s                                        
+#    mycursor.execute(sql,recds)
+#    mysql.commit()
+#    mycursor.close()
+#    print(recds)
+#    mysql.close()
+#mycursor.close()
+#mysql.close()
---
+++ ML_table.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 01 23:28:18 2019
+
+@author: Yousuf
+"""
+
+frenns='FRN100000570'
+reg='04378201'
+namez='BP P.L.C'
+import numpy as np
+import pandas as pd
+import mysql.connector
+import pandas as pd
+from sklearn import model_selection
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+mysql = mysql.connector.connect(
+  host="localhost",#"144.76.137.232",
+  user="vkingsol_demo",
+  passwd="gUj3z5?9",
+  database = "vkingsol_frennsdevelopment"
+  )
+df=pd.read_sql('select amount,vat_amount,outstanding_amount,paid from syncinvoice', con=mysql)
+x=pd.read_sql('select amount, vat_amount, outstanding_amount from syncinvoice', con=mysql)
+frn=pd.read_sql('select frenns_id from syncinvoice', con=mysql)
+df.paid=df.paid.replace([''],1)
+df.paid=df.paid.replace(['false'],0)
+df.paid=df.paid.replace(['true'],1)
+X=df.iloc[:,:3]
+Y=df.iloc[:,3]
+F=frn.iloc[:,:1]
+validation_size = 0.11
+seed=7
+#X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+#scoring = 'accuracy'
+#models = []
+#models.append(('LR', LogisticRegression()))
+#models.append(('KNN', KNeighborsClassifier(n_neighbors=5)))
+#models.append(('SVM', SVC(kernel='linear', C=0.01)))
+#results = []
+#names = []
+#for name, model in models:
+#	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+#	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+#	results.append(cv_results)
+#	names.append(name)
+#	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+##	print(msg)
+#svc = SVC()
+#svc.fit(X_train, Y_train)
+#predictions = svc.predict(X_validation)
+#print('Support Vector Machine')
+#print(accuracy_score(Y_validation, predictions))
+#print(confusion_matrix(Y_validation, predictions))
+
+x1=df.iloc[1:2,:3]
+#pr=svc.predict(x1)
+L=X.loc[1,'amount']
+
+
+#from keras.models import Sequential
+#from keras.layers import Dense
+
+#df.paid=df.paid.replace([''],1)
+#df.paid=df.paid.replace(['false'],0)
+#X=df.iloc[:,:3]
+#from keras.utils import to_categorical
+#one-hot encode target column
+#Y = to_categorical(df.iloc[:,3])
+
+#vcheck that target column has been converted
+#train_y_2[0:5]
+#Y=df.iloc[:,3]
+#
+#
+##create model
+#model_2 = Sequential()
+#
+##get number of columns in training data
+#n_cols_2 = train_X_2.shape[1]
+#
+##add layers to model
+#model_2.add(Dense(250, activation='relu', input_shape=(n_cols_2,)))
+#model_2.add(Dense(250, activation='relu'))
+#model_2.add(Dense(250, activation='relu'))
+#model_2.add(Dense(2, activation='softmax'))
+#
+#model_2.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+#model_2.fit(X_2, target, epochs=30, validation_split=0.2, callbacks=[early_stopping_monitor])
+
+#for i in range(0,len(X)):
+#    mycursor=mysql.cursor()
+#    rec=[str(i+1),str(F.loc[i,'frenns_id']),str(X.loc[i,'amount']),str(X.loc[i,'vat_amount']),str(X.loc[i,'outstanding_amount']),str(Y.iloc[i]),'yes' if Y.iloc[i]==1 else 'no']
+#    uu=pd.read_sql('select amount from ML where id={0}'.format(i+1), con=mysql)
+#    try:
+#        upd=uu['amount']
+#    except:
+#        upd=0
+#    if len(upd)==0:
+#        sql="INSERT INTO ML(id, frenns_id, amount, vat_amount, outstanding_amount, class,decision) VALUES (%s,%s,%s,%s,%s,%s,%s)";mycursor.execute(sql,rec);mysql.commit()
+#mycursor.close()
+#mysql.close()
+
+#inx={'amount':[1],'vat_amount':[2],'outstanding_amount':[3],'paid':[0]}
+#inp=pd.DataFrame(data=inx)
+#
+#ml=pd.read_sql('select frenns_id from ML', con=mysql)
+#j=0
+for i in range(0,len(X)):
+    mycursor=mysql.cursor()
+#    rec=[str(i+1),str(F.loc[i,'frenns_id']),str(X.loc[i,'amount']),str(X.loc[i,'vat_amount']),str(X.loc[i,'outstanding_amount']),str(Y.iloc[i]),'yes' if Y.iloc[i]==1 else 'no']
+    uu=pd.read_sql('select amount from ML where id={0}'.format(i+1), con=mysql)
+    try:
+        upd=uu['amount']
+    except:
+        upd=0
+    
+    benford1=pd.read_sql('select value from syncfraudanalysis where area="Benfords law 1st digit" and frenns_id="{0}"'.format(F.loc[i,'frenns_id']), con=mysql)
+    benford2=pd.read_sql('select value from syncfraudanalysis where area="Benfords law 2nd digit" and frenns_id="{0}"'.format(F.loc[i,'frenns_id']), con=mysql)
+    beneish=pd.read_sql('select value from syncfraudanalysis where area="Beneish-M Score" and frenns_id="{0}"'.format(F.loc[i,'frenns_id']), con=mysql)
+    zscore=pd.read_sql('select value from syncfraudanalysis where area="Z Score" and frenns_id="{0}"'.format(F.loc[i,'frenns_id']), con=mysql)
+    benford1=benford1.mean().values
+    benford2=benford2.mean().values
+    beneish=beneish.mean().values
+    zscore=zscore.mean().values
+    if beneish<=-2.22:
+        scorebeneish=1
+    else:
+        scorebeneish=0
+    if benford1>=15:
+        scorebrad1=0
+    else:
+        scorebrad1=1
+    if benford2>=15:
+        scorebrad2=0
+    else:
+        scorebrad2=1
+    if zscore<=2:
+        scorez=1
+    else:
+        scorez=0
+#    import clearsight_final
+#    clearsight=clearsight_final.regnum()
+    credit=pd.read_sql('select avg(creditscore) as creditscore,avg(purchasescore) as purchasescore from syncfinancialanalysis where frenns_id="{0}"'.format(F.loc[i,'frenns_id']), con=mysql)
+    Credit_Score=credit.creditscore.values
+    
+    Purchase_Score=credit.purchasescore.values
+
+    if 1<Credit_Score<=470:
+        CreditRating=0;
+    elif 471<=Credit_Score<=519:
+        CreditRating=0;
+    elif 520<=Credit_Score<=568:
+        CreditRating=0;
+    elif 569<=Credit_Score<=617:
+        CreditRating=0;
+    elif 618<=Credit_Score<=666:
+        CreditRating=1;
+    elif 667<=Credit_Score<=715:
+        CreditRating=1;
+    elif 716<=Credit_Score<=764:
+        CreditRating=1;
+    elif 765<=Credit_Score<=813:
+        CreditRating=2;
+    else:
+        CreditRating=0
+    if 1<Purchase_Score<=300:
+        PurchaseRating=0;
+    elif 301<=Purchase_Score<=335:
+        PurchaseRating=0;
+    elif 336<=Purchase_Score<=371:
+        PurchaseRating=0;
+    elif 372<=Purchase_Score<=406:
+        PurchaseRating=0;
+    elif 407<=Purchase_Score<=441:
+        PurchaseRating=0;
+    elif 442<=Purchase_Score<=477:
+        PurchaseRating=1;
+    elif 478<=Purchase_Score<=512:
+        PurchaseRating=1;
+    elif 513<=Purchase_Score<=548:
+        PurchaseRating=1;
+    else:
+        PurchaseRating=0 
+    score=scorebeneish+scorebrad1+scorebrad2+scorez+CreditRating+PurchaseRating
+    if score==0 or score==1 or score==2 or score==3 or score==4:
+        cls=0
+    else:
+        cls=1
+    
+    rec=[str(i+1),str(F.loc[i,'frenns_id']),str(X.loc[i,'amount']),str(X.loc[i,'vat_amount']),str(X.loc[i,'outstanding_amount']),str(cls),'yes' if cls==1 else 'no',str(score)]
+
+    if len(upd)==0:
+        sql="INSERT INTO ML(id, frenns_id, amount, vat_amount, outstanding_amount, class,decision,score) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)";mycursor.execute(sql,rec);mysql.commit()
+
+mycursor.close()
+mysql.close()
